eb4ec413a5926a0b21f6efac2cfe2b7768670bde
cemkndz <120122784+ckkurs@users.noreply.github.com>
2023-02-26 20:37:04 +0300

    Karakter_Degistime_Patika

---
+++ Karakter Değiştirme/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Karakter_Değiştirme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bir metin girin: ");
+            string input = Console.ReadLine();
+
+            string[] words = input.Split(' ');
+            string[] newWords = new string[words.Length];
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Length <= 1)
+                {
+                    newWords[i] = words[i];
+                }
+                else
+                {
+                    char firstChar = words[i][0];
+                    char lastChar = words[i][words[i].Length - 1];
+
+                    string newWord = lastChar + words[i].Substring(1, words[i].Length - 2) + firstChar;
+                    newWords[i] = newWord;
+                }
+            }
+
+            string output = string.Join(" ", newWords);
+            Console.WriteLine($"Yeni metin: {output}");
+            
+        }
+    }
+}
